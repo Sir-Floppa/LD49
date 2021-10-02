f8897b1cc06305c6f8406124c83c100c4d840515
Sir_Floppa <nicolas.donoso.ge@outlook.com>
2021-10-02 18:15:01 -0300

    Started to setting dynamic music

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public AudioSource piano1;
+    public AudioSource piano2;
+    public AudioSource palos;
+    public AudioSource corazon;
+    public AudioSource piano3;
+
+    public int sanity = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setMusic(piano2, 4);
+        setMusic(palos, 3);
+        setMusic(corazon, 2);
+        setMusic(piano3, 1);
+    }
+
+    void setMusic(AudioSource instrument, int value)
+    {
+        if(sanity <= value && instrument.volume < 1)
+        {
+            instrument.volume += 0.5f;
+        }
+    }
+}
---
+++ Assets/Scripts/ParameterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterController : MonoBehaviour
+{
+    public float targetParameter = 5f;
+
+    public FMODUnity.StudioParameterTrigger trigger;
+
+    public FMOD.Studio.EventInstance audioEvent;
+
+    private void Start()
+    {
+        audioEvent = FMODUnity.RuntimeManager.CreateInstance("event:/Sanity");
+    }
+
+    private void OnEnable()
+    {
+        targetParameter -= 1f;
+        audioEvent.getParameterByName("event:/Sanity", out targetParameter);
+        gameObject.SetActive(false);
+    }
+}
